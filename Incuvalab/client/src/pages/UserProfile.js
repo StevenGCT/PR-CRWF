03aea0392e8792623e3155ebecf755e0681e46b5
Pc-Useh <cop0028240@est.univalle.edu>
2022-06-25 19:16:35 -0400

    El Registro de campañas del usuario fue rediseñado

--- Incuvalab/client/src/pages/UserProfile.js
+++ Incuvalab/client/src/pages/UserProfile.js
@@ -1,47 +1,43 @@
-import {useUsers} from '../context/userContext';
 import {Link} from 'react-router-dom'
-import './../css/userProfile.css';
+import Accordion from 'react-bootstrap/Accordion'
+import { Button } from 'react-bootstrap'
 import NavbarLogin from "../components/header-navbar"
 import Footer from "../components/footer"
-export function UserProfile() {
+import './../css/userProfile.css'
 
-    const{ followeds, followedsCount, donated, donatedCount, projects, projectsCount } = useUsers();
+export function UserProfile() {
 
         return(
             <div className="App">
                 <NavbarLogin locale={true} />
-                <div class="card1" id="cardUser1">
-                    <h1 id='h1'>USER PROFILE</h1>
-                    <div class="card2" id="cardUser2">
-                        <div class="subject">
-                        <select name="subject" id="subject_input">
-                            <option disabled hidden selected>Followed Projects: {followedsCount}</option>
-                            {followeds.map(followed => (
-                                <option >
-                                {followed.Title}
-                                </option>
-                            ))}   
-                        </select>
-                        <select name="subject" id="subject_input">
-                            <option disabled hidden selected>Donated Projects: {donatedCount}</option>
-                            {donated.map(donate => (
-                                <option >
-                                {donate.Title}
-                                </option>
-                            ))}   
-                        </select>
-                        <select name="subject" id="subject_input">
-                            <option disabled hidden selected>My Projects: {projectsCount}</option>
-                            {projects.map(project => (
-                                <option >
-                                {project.Title}
-                                </option>
-                            ))}   
-                        </select>
-                        <Link to="/Settings">Settings</Link>
-                        </div>
-                    </div>
-                </div>
+                    <br />
+                        <h2 className="font-semibold position-relative pl-5 pb-3">Mi Registro de Campañas</h2>
+                            <div className="container position-relative p-4" id="container">
+                            <Accordion>
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Campañas a las Que Sigues</Accordion.Header>
+                                    <Accordion.Body>
+                                    Insertar componente aqui
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>Campañas a las Que Donaste</Accordion.Header>
+                                    <Accordion.Body>
+                                    Insertar componente aqui
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header>Mis Campañas</Accordion.Header>
+                                    <Accordion.Body>
+                                        Proximamente...
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                                <div className="flex justify-center m-3 pt-2">
+                                    <Button id="toSettings"><strong>Mis Datos</strong></Button>
+                                </div>
+                            </div>
+                    <br />
                 <Footer />
             </div>
         );
